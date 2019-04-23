5b19b5a86ea13de88b665a5e72eddbb0f76b73e1
feven <feventsegay6@gmail.com>
2019-04-23 17:36:45 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,12 +5,10 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
-  birthday_message=[]
+  # add your code snippet here
 birthday_kids.each do|name,age|
-birthday_message.push("Happy Birthday #{name}! You are now #{age}years old!")
+puts "Happy Birthday #{name}! You are now #{age}years old!"
 end
-birthday_message
 end
 
 
