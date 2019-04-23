bc4e9f01361d683eab3258f47cc037697e479ff9
feven <feventsegay6@gmail.com>
2019-04-23 17:29:43 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,13 +6,9 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
-  birthday_kids = {
-  "Timmy" => 9, 
-  "Sarah" => 6, 
-  "Amanda" => 27
-}
+  
 birthday_kids.each do|name,age|
-puts 
+puts "Happy Birthday #{name}! You are now #{age}years old!"
 end
 
 
