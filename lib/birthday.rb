9df07c1ad777929b03106f1e9a6fb9bac0d0f8ca
feven <feventsegay6@gmail.com>
2019-04-23 17:28:01 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,13 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
-  
+  birthday_kids = {
+  "Timmy" => 9, 
+  "Sarah" => 6, 
+  "Amanda" => 27
+}
+birthday_kids.each do|name,age|
+puts 
 end
 
 
