245036b0d4340e1c7c295b5f8d5e1d19ad5f1a95
feven <feventsegay6@gmail.com>
2019-04-23 17:50:49 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,4 +8,11 @@
 
 def select_winner(passengers)
   # add the code snippet here!
+  winner=""
+  passengers.each do|suit,name|
+  if suit==:suite_a && name.start_with?("A")
+    winner=name
+  end
+end
+winner
 end
